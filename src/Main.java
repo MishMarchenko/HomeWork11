d52f106f97c79fdb7fe19a65070ac807ac3cb413
MishMarchenko <mishmar4enko@gmail.com>
2023-07-08 22:44:16 +0300

    started

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        List<String> st = new ArrayList<>();
+        st.add("Java");
+        st.add("Python");
+        st.add("Perl");
+        st.add("C++");
+        st.add("Java");
+        st.add("C++");
+        st.add("C#");
+        System.out.println(st);
+        Set<String> set = new HashSet<>(st);
+        System.out.println(set);
+
     }
 }
