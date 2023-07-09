e4492764a9c172eb7174c8d03680b483a929e868
MishMarchenko <mishmar4enko@gmail.com>
2023-07-09 09:27:33 +0300

    second part

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,20 @@ public class Main {
         Set<String> set = new HashSet<>(st);
         System.out.println(set);
 
+
+        Map<String, Integer> contacts = new HashMap<>();
+        contacts.put("Bob Thomas", 1);
+        contacts.put("Ted Lasso", 2);
+        contacts.put("Kim Coates", 3);
+        contacts.put("Charlie Hunnam", 4);
+        contacts.put("Ron Perlman", 5);
+        contacts.put("Katey Sagal", 6);
+        contacts.put("Mark Boone Junior", 7);
+        contacts.put("Tommy Flanagan", 8);
+        contacts.put("Maggie Siff", 9);
+        contacts.put("Dayton Callie", 10);
+        for (Map.Entry<String, Integer> mapping : contacts.entrySet()){
+            System.out.println(mapping.getKey() + " " + mapping.getValue());
+        }
     }
 }
