2632506885845016623c6e59a5f0162e0da32785
MishMarchenko <mishmar4enko@gmail.com>
2023-07-10 17:18:11 +0300

    last part

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         System.out.println(st);
         Set<String> set = new HashSet<>(st);
         System.out.println(set);
-
+        System.out.println();
 
         Map<String, Integer> contacts = new HashMap<>();
         contacts.put("Bob Thomas", 1);
@@ -27,8 +27,32 @@ public class Main {
         contacts.put("Tommy Flanagan", 8);
         contacts.put("Maggie Siff", 9);
         contacts.put("Dayton Callie", 10);
-        for (Map.Entry<String, Integer> mapping : contacts.entrySet()){
+        for (Map.Entry<String, Integer> mapping : contacts.entrySet()) {
             System.out.println(mapping.getKey() + " " + mapping.getValue());
         }
+        System.out.println();
+
+            ArrayList<Integer> array = new ArrayList<>();
+            array.add(1);
+            array.add(2);
+            array.add(3);
+            array.add(5);
+            array.add(4);
+            array.add(3);
+            array.add(2);
+            array.add(1);
+            array.add(2);
+            array.add(3);
+            array.add(5);
+            System.out.println(counterCatch(array, 2));
+    }
+    public static int counterCatch(ArrayList<Integer> arrayList, int value){
+        int count = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) == value) {
+                count++;
+            }
+        }
+        return count;
     }
 }
